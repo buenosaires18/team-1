c2cc46b72362338f530d66bcb6224b12c563f85c
Martin Grabina <mgrabina@itba.edu.ar>
2018-05-25 21:49:38 -0300

    Sidebar separated, logos implemented

---
+++ dashboard-module/assets/js/dashboard.js
@@ -0,0 +1,62 @@
+// $(document).ready(function () {
+    $('.sidebar-zone').append("<div class=\"sidebar\" data-color=\"orange\">\n" +
+        "    <!--\n" +
+        "      Tip 1: You can change the color of the sidebar using: data-color=\"blue | green | orange | red | yellow\"\n" +
+        "  -->\n" +
+        "    <div class=\"logo\">\n" +
+        "        <img src=\"../assets/img/logo.png\">\n" +
+        "    </div>\n" +
+        "    <div class=\"sidebar-wrapper\">\n" +
+        "        <ul class=\"nav\">\n" +
+        "            <li class=\"active \">\n" +
+        "                <a href=\"./dashboard.html\">\n" +
+        "                    <i class=\"now-ui-icons design_app\"></i>\n" +
+        "                    <p>Dashboard</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "            <li>\n" +
+        "                <a href=\"./icons.html\">\n" +
+        "                    <i class=\"now-ui-icons education_atom\"></i>\n" +
+        "                    <p>Icons</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "            <li>\n" +
+        "                <a href=\"./map.html\">\n" +
+        "                    <i class=\"now-ui-icons location_map-big\"></i>\n" +
+        "                    <p>Maps</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "            <li>\n" +
+        "                <a href=\"./notifications.html\">\n" +
+        "                    <i class=\"now-ui-icons ui-1_bell-53\"></i>\n" +
+        "                    <p>Notifications</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "            <li>\n" +
+        "                <a href=\"./user.html\">\n" +
+        "                    <i class=\"now-ui-icons users_single-02\"></i>\n" +
+        "                    <p>User Profile</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "            <li>\n" +
+        "                <a href=\"./tables.html\">\n" +
+        "                    <i class=\"now-ui-icons design_bullet-list-67\"></i>\n" +
+        "                    <p>Table List</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "            <li>\n" +
+        "                <a href=\"./typography.html\">\n" +
+        "                    <i class=\"now-ui-icons text_caps-small\"></i>\n" +
+        "                    <p>Typography</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "            <li class=\"active-pro\">\n" +
+        "                <a href=\"./upgrade.html\">\n" +
+        "                    <i class=\"now-ui-icons arrows-1_cloud-download-93\"></i>\n" +
+        "                    <p>Upgrade to PRO</p>\n" +
+        "                </a>\n" +
+        "            </li>\n" +
+        "        </ul>\n" +
+        "    </div>\n" +
+        "</div>\n");
+
